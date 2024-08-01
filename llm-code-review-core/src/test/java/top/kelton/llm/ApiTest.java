3ee877b7f31510d0a38e0b24fd5ab68fbb9fab71
Kelton <417160807@qq.com>
2024-08-01 23:25:16 +0800

    添加测试方法

--- llm-code-review-core/src/test/java/top/kelton/llm/ApiTest.java
+++ llm-code-review-core/src/test/java/top/kelton/llm/ApiTest.java
@@ -1,4 +1,14 @@
 package top.kelton.llm;
 
+import top.kelton.llm.component.git.GitCommand;
+
+import java.io.IOException;
+
 public class ApiTest {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        GitCommand gitCommand = new GitCommand();
+        String diff = gitCommand.diff();
+        System.out.println(diff);
+    }
 }
