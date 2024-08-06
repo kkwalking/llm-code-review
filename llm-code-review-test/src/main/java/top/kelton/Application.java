5f47a27d6fe1adcc441720e3b29a8b88b7355f3c
zhouzekun <zhouzk3@chinatelecom.cn>
2024-08-06 10:42:25 +0800

    修复github action4

--- llm-code-review-test/src/main/java/top/kelton/Application.java
+++ llm-code-review-test/src/main/java/top/kelton/Application.java
@@ -1,9 +1,16 @@
 package top.kelton;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author zzk
  * @description
  * @created 2024/8/6
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
 }
